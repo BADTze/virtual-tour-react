57451cad82067f9a4971d3b02765cb393a716dd2
Imam541 <80622187+BADTze@users.noreply.github.com>
2024-03-01 16:01:08 +0700

    Update_Link

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,16 +6,16 @@ export const arr: IArea[] = [
     slug: 'ring-1',
     rooms: [
       {
-        url: 'https://momento360.com/e/u/e4b16f1137f04cd18af6d8a6586b2a72?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true',
-        label: 'Ruangan 1.1',
+        url: 'https://momento360.com/e/u/49bace9b03c745aab78bc07dc1eba5c5?utm_campaign=embed&utm_source=other&heading=151.12&pitch=-4.31&field-of-view=75&size=medium&display-plan=true',
+        label: 'WTP Room 1',
       },
       {
-        url: 'https://momento360.com/e/u/95e46dae2f6748fcae65a942656af37b?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true',
-        label: 'Ruangan 1.2',
+        url: 'https://momento360.com/e/u/44f411fb15b54100b4c006d39a8aaad2?utm_campaign=embed&utm_source=other&heading=-39.5&pitch=-2.05&field-of-view=100&size=medium&display-plan=true',
+        label: 'Boiler Room',
       },
       {
-        url: 'https://momento360.com/e/u/7574bc1982b94bdda7070a847b4de577?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true',
-        label: 'Ruangan 1.3',
+        url: 'https://momento360.com/e/u/9d13a1a41c2f4d059cf112585247e00e?utm_campaign=embed&utm_source=other&heading=-41.82&pitch=-4.37&field-of-view=100&size=medium&display-plan=true',
+        label: 'Pump Room',
       },
     ],
   },
