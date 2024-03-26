dab4d5fda16a206d8469f80000db3ca9c50996f6
Imam541 <80622187+BADTze@users.noreply.github.com>
2024-03-26 15:06:29 +0700

    Adding r2 Outer

--- src/constants/outer.ts
+++ src/constants/outer.ts
@@ -29,7 +29,7 @@ export const Outer: IArea = {
             pitch: -0.03,
           },
           tooltip: {
-            content: "",
+            content: "<b>WTP Room</b>",
           },
         },
         {
@@ -40,12 +40,12 @@ export const Outer: IArea = {
             pitch: 0.014,
           },
           tooltip: {
-            content: "",
+            content: "<b>Compressor Room</b>",
           },
         },
         {
           markerType: "pin",
-          id: "/outer/20240301_140256_101",
+          id: "/outer/20240304_132430_068",
           position: {
             yaw: 1.504,
             pitch: 0.018,
@@ -421,5 +421,166 @@ export const Outer: IArea = {
         },
       ],
     },
-  ]
+    // Ring 2 Outer List
+    {
+      url: "",
+      label: "",
+      slug: "20240304_132430_068",
+      markers: [
+        {
+          markerType: "pin",
+          id: "outer/20240301_140256_101",
+          position: {
+            yaw: 0.151,
+            pitch: 0.005,
+          },
+          tooltip: {
+            content: "Menuju Ring 1",
+          },
+        },
+        {
+          markerType: "enter",
+          id: "ring-2/wtp-room-2",
+          position: {
+            yaw: 4.58,
+            pitch: -0.026,
+          },
+          tooltip: {
+            content: "<b>WTP Room</b>",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "outer/20240304_132445_909",
+          position: {
+            yaw: 3.316,
+            pitch: -0.06,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+      ],
+    },
+    {
+      url: "",
+      label: "",
+      slug: "20240304_132445_909",
+      markers: [
+        {
+          markerType: "pin",
+          id: "outer/20240304_132430_068",
+          position: {
+            yaw: 1.686,
+            pitch: -0.039,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "outer/20240304_105045_706",
+          position: {
+            yaw: 6.131,
+            pitch: -0.055,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+      ],
+    },
+    {
+      url: "",
+      label: "",
+      slug: "20240304_105045_706",
+      markers: [
+        {
+          markerType: "pin",
+          id: "20240304_132445_909",
+          position: {
+            yaw: 1.474,
+            pitch: -0.015,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+        {
+          markerType: "enter",
+          id: "ring-2/cooling-twr",
+          position: {
+            yaw: 1.079,
+            pitch: 0.095,
+          },
+          tooltip: {
+            content: "<b>Cooling Tower Area</b>",
+          },
+        },
+        {
+          markerType: "enter",
+          id: "ring-2/comp-low",
+          position: {
+            yaw: 5.933,
+            pitch: 0.002,
+          },
+          tooltip: {
+            content: "<b>Compressor Low Room</b>",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "outer/20240226_104430_347",
+          position: {
+            yaw: 4.557,
+            pitch: -0.116,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+      ],
+    },
+    {
+      url: "",
+      label: "",
+      slug: "20240226_104430_347",
+      markers: [
+        {
+          markerType: "pin",
+          id: "outer/20240304_105045_706",
+          position: {
+            yaw: 1.75,
+            pitch: -0.064,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "ring-2/substation-oc3",
+          position: {
+            yaw: 0.037,
+            pitch: 0.063,
+          },
+          tooltip: {
+            content: "<b>Substation Oc 3</b>",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "outer",
+          position: {
+            yaw: 4.482,
+            pitch: -0.148,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+      ],
+    },
+  ],
 };
