f3722a29beb1c0c608589d0844bcefa3de6080a7
Imam541 <80622187+BADTze@users.noreply.github.com>
2024-03-26 12:45:35 +0700

    adding ts outer

--- src/constants/outer.ts
+++ src/constants/outer.ts
@@ -12,28 +12,414 @@ export const Outer: IArea = {
         {
           markerType: "walk",
           arrowType: "left",
-          id: "/",
+          id: "/outer/20240301_140318_290",
           position: {
             yaw: 4.479,
             pitch: -0.078,
           },
           tooltip: {
-            content: "Go <b>WTP</b>",
+            content: "Menuju",
           },
         },
         {
           markerType: "enter",
-          //   arrowType: "left",
           id: "ring-1/wtp-room-1",
           position: {
             yaw: 2.911,
             pitch: -0.03,
           },
           tooltip: {
-            content: "Go To <b>WTP</b>",
+            content: "",
+          },
+        },
+        {
+          markerType: "enter",
+          id: "ring-1/compressor-room",
+          position: {
+            yaw: 2.227,
+            pitch: 0.014,
+          },
+          tooltip: {
+            content: "",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "/outer/20240301_140256_101",
+          position: {
+            yaw: 1.504,
+            pitch: 0.018,
+          },
+          tooltip: {
+            content: "Menuju ke Ring 2",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "/b",
+          position: {
+            yaw: 5.889,
+            pitch: -0.089,
+          },
+          tooltip: {
+            content: "Kembali",
           },
         },
       ],
     },
-  ],
+    {
+      url: "",
+      label: "",
+      slug: "20240301_140318_290",
+      markers: [
+        {
+          markerType: "pin",
+          id: "/outer/20240305_142027_547",
+          position: {
+            yaw: 6.234,
+            pitch: -0.039,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "/outer/20240301_140256_101",
+          position: {
+            yaw: 4.635,
+            pitch: -0.061,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+        {
+          markerType: "enter",
+          id: "/ring-1/sub-station1.2",
+          position: {
+            yaw: 4.247,
+            pitch: -0.033,
+          },
+          tooltip: {
+            content: "<b>Subtation room</b>",
+          },
+        },
+        {
+          markerType: "enter",
+          id: "/ring-1/genset-1",
+          position: {
+            yaw: 4.079,
+            pitch: -0.025,
+          },
+          tooltip: {
+            content: "<b>Genset Room</b>",
+          },
+        },
+      ],
+    },
+    {
+      url: "",
+      label: "",
+      slug: "20240305_142027_547",
+      markers: [
+        {
+          markerType: "pin",
+          id: "/outer/20240301_140318_290",
+          position: {
+            yaw: 4.623,
+            pitch: -0.068,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+        {
+          markerType: "enter",
+          id: "/ring-1/solar-tank",
+          position: {
+            yaw: 3.592,
+            pitch: 0.007,
+          },
+          tooltip: {
+            content: "<b>Solar Tank</b>",
+          },
+        },
+        {
+          markerType: "enter",
+          id: "/ring-1/recycle-water-room",
+          position: {
+            yaw: 2.678,
+            pitch: -0.063,
+          },
+          tooltip: {
+            content: "<b>Water Recycle Area</b>",
+          },
+        },
+        {
+          markerType: "enter",
+          id: "/ring-1/boiler-1",
+          position: {
+            yaw: 2.058,
+            pitch: 0.003,
+          },
+          tooltip: {
+            content: "<b>Boiler Room</b>",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "/outer/20240301_140645_472",
+          position: {
+            yaw: 1.485,
+            pitch: -0.087,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+      ],
+    },
+    {
+      url: "",
+      label: "",
+      slug: "20240301_140645_472",
+      markers: [
+        {
+          markerType: "pin",
+          id: "/outer/20240305_142027_547",
+          position: {
+            yaw: 1.412,
+            pitch: -0.081,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+        {
+          markerType: "walk",
+          arrowType: "left",
+          id: "/ring-1/pump-room-1",
+          position: {
+            yaw: 5.915,
+            pitch: 0.002,
+          },
+          tooltip: {
+            content: "<b>Pump Room 1</b>",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "/outer/20240301_140732_269",
+          position: {
+            yaw: 4.376,
+            pitch: -0.085,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+      ],
+    },
+    {
+      url: "",
+      label: "",
+      slug: "20240301_140732_269",
+      markers: [
+        {
+          markerType: "pin",
+          id: "/outer/20240301_140645_472",
+          position: {
+            yaw: 1.412,
+            pitch: -0.081,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "/outer/20240301_154427_324",
+          position: {
+            yaw: 6.174,
+            pitch: -0.056,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "/ring-1/wwtp-office",
+          position: {
+            yaw: 5.383,
+            pitch: 0.239,
+          },
+          tooltip: {
+            content: "<b>WWTP Office</b>",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "/outer/20240301_140752_414",
+          position: {
+            yaw: 4.587,
+            pitch: -0.085,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+      ],
+    },
+    {
+      url: "",
+      label: "",
+      slug: "20240301_154427_324",
+      markers: [
+        {
+          markerType: "pin",
+          id: "/ring-1/dw-2",
+          position: {
+            yaw: 0.428,
+            pitch: -0.097,
+          },
+          tooltip: {
+            content: "<b>Deep Well 2</b>",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "/outer/20240301_140732_269",
+          position: {
+            yaw: 4.599,
+            pitch: -0.091,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "/ring-1/wwtp-area-1",
+          position: {
+            yaw: 2.375,
+            pitch: 0.371,
+          },
+          tooltip: {
+            content: "<b>WWTP Area</b>",
+          },
+        },
+      ],
+    },
+    {
+      url: "",
+      label: "",
+      slug: "20240301_140752_414",
+      markers: [
+        {
+          markerType: "pin",
+          id: "/outer/20240301_154741_359",
+          position: {
+            yaw: 1.719,
+            pitch: -0.025,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "/outer/20240301_140732_269",
+          position: {
+            yaw: 5.687,
+            pitch: -0.059,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+      ],
+    },
+    {
+      url: "",
+      label: "",
+      slug: "20240301_154741_359",
+      markers: [
+        {
+          markerType: "pin",
+          id: "/outer/20240301_140752_414",
+          position: {
+            yaw: 2.154,
+            pitch: -0.042,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+        {
+          markerType: "enter",
+          id: "/ring-1/sub-station1.1",
+          position: {
+            yaw: 0.214,
+            pitch: 0.011,
+          },
+          tooltip: {
+            content: "<b>Substation</b>",
+          },
+        },
+        {
+          markerType: "enter",
+          id: "/ring-1/chiller-gbl",
+          position: {
+            yaw: 5.91,
+            pitch: 0.088,
+          },
+          tooltip: {
+            content: "<b>Chiller GBL</b>",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "/ring-1/dw-1",
+          position: {
+            yaw: 4.776,
+            pitch: -0.049,
+          },
+          tooltip: {
+            content: "<b>Deep Well Area</b>",
+          },
+        },
+      ],
+    },
+    {
+      url: "",
+      label: "",
+      slug: "20240304_085609_640",
+      markers: [
+        {
+          markerType: "pin",
+          id: "/ring-1/dw-1",
+          position: {
+            yaw: 2.237,
+            pitch: -0.074,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+        {
+          markerType: "pin",
+          id: "/outer/",
+          position: {
+            yaw: 0.167,
+            pitch: -0.034,
+          },
+          tooltip: {
+            content: "Menuju",
+          },
+        },
+      ],
+    },
+  ]
 };
--- src/constants/ring-1.ts
+++ src/constants/ring-1.ts
@@ -1,336 +1,358 @@
-import { IArea } from '@/types';
+import { IArea } from "@/types";
 
 export const ring1Area: IArea = {
-  name: 'Ring 1',
-  slug: 'ring-1',
+  name: "Ring 1",
+  slug: "ring-1",
   rooms: [
     {
-      url: 'https://momento360.com/e/u/49bace9b03c745aab78bc07dc1eba5c5?utm_campaign=embed&utm_source=other&heading=151.12&pitch=-4.31&field-of-view=75&size=medium&display-plan=true',
-      label: 'WTP Room 1',
-      slug: 'wtp-room-1',
+      url: "https://momento360.com/e/u/49bace9b03c745aab78bc07dc1eba5c5?utm_campaign=embed&utm_source=other&heading=151.12&pitch=-4.31&field-of-view=75&size=medium&display-plan=true",
+      label: "WTP Room 1",
+      slug: "wtp-room-1",
       markers: [
         {
-          markerType: 'walk',
-          arrowType:'right',
-          id: 'outer/20240301_140256_101',
+          markerType: "walk",
+          arrowType: "right",
+          id: "outer/20240301_140256_101",
           position: {
-            "yaw": 4.772,
-            "pitch": -0.114
+            yaw: 4.772,
+            pitch: -0.114,
           },
           tooltip: {
-            content: 'Go <b>Testing</b>',
+            content: "Go <b>Testing</b>",
           },
         },
         {
-          markerType: 'pin',
-          id: 'ring-1/compressor-room',
-          position:{
-            "yaw": 0.99,
-            "pitch": -0.019
+          markerType: "pin",
+          id: "ring-1/compressor-room",
+          position: {
+            yaw: 0.99,
+            pitch: -0.019,
           },
           tooltip: {
-            content: 'Go to <b>Compressor Room</b>',
+            content: "Go to <b>Compressor Room</b>",
           },
         },
       ],
     },
     {
-      url: 'https://momento360.com/e/u/fb9186cac6854e2683bc0a15196466dd?utm_campaign=embed&utm_source=other&heading=-200.06&pitch=0.44&field-of-view=100&size=medium&display-plan=true',
-      label: 'Compressor Room',
-      slug: 'compressor-room',
-      markers: [
-      {
-        markerType: 'pin',
-        id: 'ring-1/wtp-room-1',
-        position: {
-          "yaw": 0.313,
-          "pitch": -0.121
-        },
-        tooltip: {
-          content: 'Go <b>WTP Room</b>',
-        },
-      },
-      {
-        markerType: 'pin',
-        id: 'ring-1/w',
-        position: {
-          "yaw": 4.563,
-          "pitch": -0.114
-        },
-        tooltip: {
-          content: 'Go <b>WTP Room</b>',
-        },
-      },
-    ],
-    },
-    {
-      url: 'https://momento360.com/e/u/fb9186cac6854e2683bc0a15196466dd?utm_campaign=embed&utm_source=other&heading=-200.06&pitch=0.44&field-of-view=100&size=medium&display-plan=true',
-      label: 'Genset Room',
-      slug: 'genset-1',
-      markers: [{
-        markerType: 'pin',
-        id: 'ring-1/',
-        position:  {
-          "yaw": 3.606,
-          "pitch": -0.059
-        },
-        tooltip: {
-          content: 'Go <b>WTP Room</b>',
-        },
-      }],
-    },
-    {
-      url: 'https://momento360.com/e/u/fb9186cac6854e2683bc0a15196466dd?utm_campaign=embed&utm_source=other&heading=-200.06&pitch=0.44&field-of-view=100&size=medium&display-plan=true',
-      label: 'Substation Room',
-      slug: 'sub-station1.2',
-      markers: [{
-        markerType: 'pin',
-        id: 'ring-1/',
-        position: {
-          "yaw": 3.176,
-          "pitch": -0.071
-        },
-        tooltip: {
-          content: 'Go <b>WTP Room</b>',
-        },
-      }],
-    },
-    {
-      url: 'https://momento360.com/e/u/f05e6454ac684437aa3ae78038b541a4?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true',
-      label: 'Solar Tank',
-      slug: 'solar-tank',
+      url: "https://momento360.com/e/u/fb9186cac6854e2683bc0a15196466dd?utm_campaign=embed&utm_source=other&heading=-200.06&pitch=0.44&field-of-view=100&size=medium&display-plan=true",
+      label: "Compressor Room",
+      slug: "compressor-room",
       markers: [
         {
-        markerType: 'pin',
-        id: 'ring-1/',
-        position: {
-          "yaw": 3.252,
-          "pitch": 0.036
-        },
-        tooltip: {
-          content: 'Keluar',
-        },
-      }
-    ],
-    },
-    {
-      url: 'https://momento360.com/e/u/44f411fb15b54100b4c006d39a8aaad2?utm_campaign=embed&utm_source=other&heading=-39.5&pitch=-2.05&field-of-view=100&size=medium&display-plan=true',
-      label: 'Boiler Room',
-      slug: 'boiler-1',
-      markers: [
-        {
-          markerType: 'pin',
-          id: 'ring-1/',
-          position:  {
-            "yaw": 2.386,
-            "pitch": -0.298
+          markerType: "pin",
+          id: "outer/20240301_140256_101",
+          position: {
+            yaw: 0.313,
+            pitch: -0.121,
           },
           tooltip: {
-            content: 'Keluar',
+            content: "Keluar",
           },
-        }
+        },
+        {
+          markerType: "pin",
+          id: "ring-1/w",
+          position: {
+            yaw: 4.563,
+            pitch: -0.114,
+          },
+          tooltip: {
+            content: "Masuk",
+          },
+        },
       ],
     },
     {
-      url: 'https://momento360.com/e/u/9d13a1a41c2f4d059cf112585247e00e?utm_campaign=embed&utm_source=other&heading=-41.82&pitch=-4.37&field-of-view=100&size=medium&display-plan=true',
-      label: 'Pump Room',
-      slug: 'pump-room-1',
+      url: "https://momento360.com/e/u/fb9186cac6854e2683bc0a15196466dd?utm_campaign=embed&utm_source=other&heading=-200.06&pitch=0.44&field-of-view=100&size=medium&display-plan=true",
+      label: "Genset Room",
+      slug: "genset-1",
       markers: [
         {
-          markerType: 'pin',
-          id: 'ring-1/',
+          markerType: "pin",
+          id: "outer/20240301_140318_290",
           position: {
-            "yaw": 4.834,
-            "pitch": -0.137
+            yaw: 3.606,
+            pitch: -0.059,
           },
           tooltip: {
-            content: 'Keluar',
+            content: "Keluar",
           },
-        }
+        },
       ],
     },
     {
-      url: 'https://momento360.com/e/u/f68a0e9421e14668b1de2f5eef21ecd2?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true',
-      label: 'Deep Well 2',
-      slug: 'dw-2',
+      url: "https://momento360.com/e/u/fb9186cac6854e2683bc0a15196466dd?utm_campaign=embed&utm_source=other&heading=-200.06&pitch=0.44&field-of-view=100&size=medium&display-plan=true",
+      label: "Substation Room",
+      slug: "sub-station1.2",
       markers: [
         {
-          markerType: 'pin',
-          id: 'ring-1/',
+          markerType: "pin",
+          id: "outer/20240301_140318_290",
           position: {
-            "yaw": 0.317,
-            "pitch": -0.113
+            yaw: 3.176,
+            pitch: -0.071,
           },
           tooltip: {
-            content: 'Keluar',
+            content: "Keluar",
           },
-        }
+        },
       ],
     },
     {
-      url: 'https://momento360.com/e/u/729693e08c764b9eb9cda09781cef464?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true',
-      label: 'WWTP Area',
-      slug: 'wwtp-area-1',
+      url: "https://momento360.com/e/u/f05e6454ac684437aa3ae78038b541a4?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true",
+      label: "Solar Tank",
+      slug: "solar-tank",
       markers: [
         {
-          markerType: 'pin',
-          id: 'ring-1/',
+          markerType: "pin",
+          id: "ring-1/",
           position: {
-            "yaw": 0.518,
-            "pitch": -0.187
+            yaw: 3.252,
+            pitch: 0.036,
           },
           tooltip: {
-            content: 'Keluar',
+            content: "Keluar",
+          },
+        },
+      ],
+    },
+    {
+      url: "https://momento360.com/e/u/f05e6454ac684437aa3ae78038b541a4?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true",
+      label: "Recycle Water",
+      slug: "recycle-water-room",
+      markers: [
+        {
+          markerType: "pin",
+          id: "ring-1/",
+          position: {
+            yaw: 3.721,
+            pitch: -0.107,
+          },
+          tooltip: {
+            content: "Keluar",
+          },
+        },
+      ],
+    },
+    {
+      url: "https://momento360.com/e/u/44f411fb15b54100b4c006d39a8aaad2?utm_campaign=embed&utm_source=other&heading=-39.5&pitch=-2.05&field-of-view=100&size=medium&display-plan=true",
+      label: "Boiler Room",
+      slug: "boiler-1",
+      markers: [
+        {
+          markerType: "pin",
+          id: "ring-1/",
+          position: {
+            yaw: 2.386,
+            pitch: -0.298,
+          },
+          tooltip: {
+            content: "Keluar",
+          },
+        },
+      ],
+    },
+    {
+      url: "https://momento360.com/e/u/9d13a1a41c2f4d059cf112585247e00e?utm_campaign=embed&utm_source=other&heading=-41.82&pitch=-4.37&field-of-view=100&size=medium&display-plan=true",
+      label: "Pump Room",
+      slug: "pump-room-1",
+      markers: [
+        {
+          markerType: "pin",
+          id: "ring-1/",
+          position: {
+            yaw: 4.834,
+            pitch: -0.137,
+          },
+          tooltip: {
+            content: "Keluar",
+          },
+        },
+      ],
+    },
+    {
+      url: "https://momento360.com/e/u/f68a0e9421e14668b1de2f5eef21ecd2?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true",
+      label: "Deep Well 2",
+      slug: "dw-2",
+      markers: [
+        {
+          markerType: "pin",
+          id: "ring-1/",
+          position: {
+            yaw: 0.317,
+            pitch: -0.113,
+          },
+          tooltip: {
+            content: "Keluar",
+          },
+        },
+      ],
+    },
+    {
+      url: "https://momento360.com/e/u/729693e08c764b9eb9cda09781cef464?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true",
+      label: "WWTP Area",
+      slug: "wwtp-area-1",
+      markers: [
+        {
+          markerType: "pin",
+          id: "ring-1/",
+          position: {
+            yaw: 0.518,
+            pitch: -0.187,
+          },
+          tooltip: {
+            content: "Keluar",
           },
         },
         {
-          markerType: 'pin',
-          id: 'ring-1/water-storage',
+          markerType: "pin",
+          id: "ring-1/water-storage",
           position: {
-            "yaw": 4.957,
-            "pitch": -0.191
+            yaw: 4.957,
+            pitch: -0.191,
           },
           tooltip: {
-            content: 'Masuk',
+            content: "Masuk",
           },
-        }
+        },
       ],
     },
     {
-      url: 'https://momento360.com/e/u/d376aaf2a2b34709a6dccf81b18e7799?utm_campaign=embed&utm_source=other&heading=-86.85&pitch=-0.19&field-of-view=75&size=medium&display-plan=true',
-      label: 'Water Storage',
-      slug: 'water-storage',
+      url: "https://momento360.com/e/u/d376aaf2a2b34709a6dccf81b18e7799?utm_campaign=embed&utm_source=other&heading=-86.85&pitch=-0.19&field-of-view=75&size=medium&display-plan=true",
+      label: "Water Storage",
+      slug: "water-storage",
       markers: [
         {
-          markerType: 'pin',
-          id: 'ring-1/',
+          markerType: "pin",
+          id: "ring-1/",
           position: {
-            "yaw": 4.086,
-            "pitch": -0.208
+            yaw: 4.086,
+            pitch: -0.208,
           },
           tooltip: {
-            content: 'Keluar',
+            content: "Keluar",
           },
-        }
+        },
       ],
     },
     {
-      url: 'https://momento360.com/e/u/d376aaf2a2b34709a6dccf81b18e7799?utm_campaign=embed&utm_source=other&heading=-86.85&pitch=-0.19&field-of-view=75&size=medium&display-plan=true',
-      label: 'WWTP Office',
-      slug: 'wwtp-office',
+      url: "https://momento360.com/e/u/d376aaf2a2b34709a6dccf81b18e7799?utm_campaign=embed&utm_source=other&heading=-86.85&pitch=-0.19&field-of-view=75&size=medium&display-plan=true",
+      label: "WWTP Office",
+      slug: "wwtp-office",
       markers: [
         {
-          markerType: 'pin',
-          id: 'ring-1/',
+          markerType: "pin",
+          id: "ring-1/",
           position: {
-            "yaw": 1.793,
-            "pitch": -0.125
+            yaw: 1.793,
+            pitch: -0.125,
           },
           tooltip: {
-            content: 'Keluar',
+            content: "Keluar",
           },
-        }
+        },
       ],
     },
     {
-      url: 'https://momento360.com/e/u/377fd915dd8043008d5549f39e5da1b6?utm_campaign=embed&utm_source=other&heading=-149.42&pitch=-4.65&field-of-view=75&size=medium&display-plan=true',
-      label: 'Substation Room',
-      slug: 'sub-station1.1',
+      url: "https://momento360.com/e/u/377fd915dd8043008d5549f39e5da1b6?utm_campaign=embed&utm_source=other&heading=-149.42&pitch=-4.65&field-of-view=75&size=medium&display-plan=true",
+      label: "Substation Room",
+      slug: "sub-station1.1",
       markers: [
         {
-          markerType: 'pin',
-          id: 'ring-1/',
+          markerType: "pin",
+          id: "ring-1/",
           position: {
-            "yaw": 6.064,
-            "pitch": -0.108
+            yaw: 6.064,
+            pitch: -0.108,
           },
           tooltip: {
-            content: 'Keluar',
+            content: "Keluar",
           },
-        }
+        },
       ],
     },
     {
-      url: 'https://momento360.com/e/u/2ed29b0ef740450daa0e8a8025153dd7?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true',
-      label: 'Chiller GBL',
-      slug: 'chiller-gbl',
+      url: "https://momento360.com/e/u/2ed29b0ef740450daa0e8a8025153dd7?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true",
+      label: "Chiller GBL",
+      slug: "chiller-gbl",
       markers: [
         {
-          markerType: 'pin',
-          id: 'ring-1/',
+          markerType: "pin",
+          id: "ring-1/",
           position: {
-            "yaw": 3.431,
-            "pitch": -0.119
+            yaw: 3.431,
+            pitch: -0.119,
           },
           tooltip: {
-            content: 'Keluar',
+            content: "Keluar",
           },
-        }
+        },
       ],
     },
     {
-      url: 'https://momento360.com/e/u/15c499b4a0fc4f04a43d5cf217a42a76?utm_campaign=embed&utm_source=other&heading=35.8&pitch=-3.6&field-of-view=100&size=medium&display-plan=true',
-      label: 'Deep Well 1',
-      slug: 'dw-1',
+      url: "https://momento360.com/e/u/15c499b4a0fc4f04a43d5cf217a42a76?utm_campaign=embed&utm_source=other&heading=35.8&pitch=-3.6&field-of-view=100&size=medium&display-plan=true",
+      label: "Deep Well 1",
+      slug: "dw-1",
       markers: [
         {
-          markerType: 'pin',
-          id: 'ring-1/',
+          markerType: "pin",
+          id: "outer/20240304_085609_640",
           position: {
-            "yaw": 4.62,
-            "pitch": -0.126
+            yaw: 4.62,
+            pitch: -0.126,
           },
           tooltip: {
-            content: 'Keluar',
+            content: "Menuju",
           },
         },
         {
-          markerType: 'pin',
-          id: 'ring-1/d',
+          markerType: "pin",
+          id: "/outer/20240301_154741_359",
           position: {
-            "yaw": 1.868,
-            "pitch": -0.061
+            yaw: 1.868,
+            pitch: -0.061,
           },
           tooltip: {
-            content: 'Keluar',
+            content: "Kembali",
           },
-        }
+        },
       ],
     },
     {
-      url: 'https://momento360.com/e/u/581b48a465de426988a8384340fe20ab?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true',
-      label: 'Deep Well 5',
-      slug: 'dw-5',
+      url: "https://momento360.com/e/u/581b48a465de426988a8384340fe20ab?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true",
+      label: "Deep Well 5",
+      slug: "dw-5",
       markers: [
         {
-          markerType: 'pin',
-          id: 'ring-1/',
+          markerType: "pin",
+          id: "ring-1/",
           position: {
-            "yaw": 5.826,
-            "pitch": -0.612
+            yaw: 5.826,
+            pitch: -0.612,
           },
           tooltip: {
-            content: 'Keluar',
+            content: "Keluar",
           },
-        }
+        },
       ],
     },
     {
-      url: 'https://momento360.com/e/u/894529582f374eeca39248bf50c766b1?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true',
-      label: 'Deep Well 3',
-      slug: 'dw-3',
+      url: "https://momento360.com/e/u/894529582f374eeca39248bf50c766b1?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true",
+      label: "Deep Well 3",
+      slug: "dw-3",
       markers: [
         {
-          markerType: 'pin',
-          id: 'ring-1/',
+          markerType: "pin",
+          id: "ring-1/",
           position: {
-            "yaw": 4.492,
-            "pitch": 0.316
+            yaw: 4.492,
+            pitch: 0.316,
           },
           tooltip: {
-            content: 'Keluar',
+            content: "Keluar",
           },
-        }
+        },
       ],
     },
   ],
